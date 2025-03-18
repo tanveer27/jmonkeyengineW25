7bd0565e563f5def9aee3aaa1b13fa687c7c06d1
YASH KOLADIYA <90193777+Ashy09@users.noreply.github.com>
2025-03-17 20:26:39 -0400

    Update MjpegFileWriter.java

--- jme3-android/src/main/java/com/jme3/app/state/MjpegFileWriter.java
+++ jme3-android/src/main/java/com/jme3/app/state/MjpegFileWriter.java
@@ -42,6 +42,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import com.example.utils.MjpegUtils;  
+
 
 /**
  * Released under BSD License
@@ -486,7 +488,7 @@ public class MjpegFileWriter {
 
             baos.close();
 
-            return baos.toByteArray();
+            return MjpegUtils.toByteArray(baos);
         }
     }
 
